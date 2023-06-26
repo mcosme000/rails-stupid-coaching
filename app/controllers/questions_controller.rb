2fe6e5650b3610b356e06733790a0e456265ae4d
Maria Cosmen <mcosme00@gmail.com>
2023-06-26 22:42:21 +0900

    Move logic to controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,7 +3,14 @@ class QuestionsController < ApplicationController
   def ask; end
 
   def answer
-    params
-    @favorite_sentence = 'I am going to work'
+    @question = params[:question]
+    @favorite_sentence = "I am going to work"
+    if @question == @favorite_sentence
+      @answer = "Great!"
+    elsif @question.ends_with?("?")
+      @answer = "Silly question, get dressed and go to work!"
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
   end
 end
